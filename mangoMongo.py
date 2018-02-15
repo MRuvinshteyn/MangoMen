698570f0de8e1ffd615b3a4fb1beab86992de228
Michael Ruvinshteyn <mruvinshteyn@stuy.edu>
2018-02-14 20:28:10 -0500

    tangoMango

--- mangoMongo.py
+++ mangoMongo.py
@@ -1,1 +1,33 @@
 from pymongo import MongoClient
+
+print_results = True #print items individually or return them as a list?
+
+c = MongoClient("lisa.stuy.edu")
+db = c.test
+collection = db.restaurants
+
+#for i in collection.find():
+    #print len(i['grades'])
+
+    
+def findRestaurantsByBorough(borough):
+    restaurants = collection.find({"borough":borough})
+    if (print_results):
+        for i in restaurants:
+            print i
+    else:
+        #print restaurants
+        return restaurants
+
+#findRestaurantsByBorough("Queens")
+
+def findRestaurantsByZipcode(zipcode):
+    restaurants = collection.find({"address.zipcode":zipcode})
+    if (print_results):
+        for i in restaurants:
+            print i
+    else:
+        print restaurants
+        return restaurants
+
+#findRestaurantsByZipcode("11377")
